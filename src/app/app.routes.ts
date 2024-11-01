163215b892385692051dfc8b4d1a3dad7d09ebc2
sutapaaditya <sutapaaditya84@gmail.com>
2024-11-01 14:15:49 +0530

    Balaji phot frames started

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'gallery',
+    component: GalleryComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  }
+];
