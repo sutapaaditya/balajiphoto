8423eae76dd00f5cf84c1d19edbbb28bf941bad2
barungh <barungh@hotmail.com>
2024-11-02 18:12:18 +0530

    contact and home responsive design

--- src/app/homegallery/homegallery.component.ts
+++ src/app/homegallery/homegallery.component.ts
@@ -13,7 +13,7 @@ export class HomegalleryComponent {
   image = '../../assets/homegallery'
 
   constructor() {
-    for (let i = 1; i <= 14; i++) {
+    for (let i = 1; i <= 13; i++) {
       this.imagePaths.push(`../../assets/homegallery/img${i}.jpg`); // Assuming images are named image1.jpg, image2.jpg, etc.
     }
   }
